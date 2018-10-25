663346065d9b225033de47a73a9efc2856837c3f
Rodchananat <rod8711@gmail.com>
2018-10-26 01:13:26 +0700

    :sparkles: now can write mqtt data into influxDB

--- common/common.go
+++ common/common.go
@@ -3,6 +3,7 @@ package common
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/influxdata/influxdb/client/v2"
@@ -51,12 +52,60 @@ func ParseJSON(payload []byte) gin.H {
 }
 
 // ConnectToInfluxDB : connect to influx DB and return client
-func ConnectToInfluxDB() client.Client {
+func ConnectToInfluxDB() (client.Client, error) {
 	influxConn, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     "http://localhost:8086",
 		Username: "admin",
 		Password: "4fs,mg-0zv",
 	})
 	CheckErr("Influx Connection", err)
-	return influxConn
+	return influxConn, err
+}
+
+// QueryInfluxDB : runs query in influxDB
+func QueryInfluxDB(query string) []client.Result {
+	clnt, err := ConnectToInfluxDB()
+
+	CheckErr("connect to query influx", err)
+	if err == nil {
+		resp, err := clnt.Query(client.Query{
+			Command:  query,
+			Database: "CUSmartFarm",
+		})
+		CheckErr("querying influx", err)
+		if err == nil {
+			fmt.Printf("Query Success: %v \n", resp)
+		}
+		return resp.Results
+	}
+	return []client.Result{}
+}
+
+// WriteInfluxDB : Write a data point in to influxDB
+func WriteInfluxDB(measurement string, tags map[string]string, fields map[string]interface{}) error {
+	clnt, err := ConnectToInfluxDB()
+
+	CheckErr("connect to query influx", err)
+	if err == nil {
+		batchPoints, err := client.NewBatchPoints(client.BatchPointsConfig{
+			Database:  "CUSmartFarm",
+			Precision: "ms",
+		})
+		if CheckErr("create batch points", err) {
+			return err
+		}
+
+		point, err := client.NewPoint("air_sensor", tags, fields, time.Now())
+		if CheckErr("create new point", err) {
+			return err
+		}
+
+		batchPoints.AddPoint(point)
+		err = clnt.Write(batchPoints)
+		if !CheckErr("querying influx", err) {
+			fmt.Printf("DB Write Succeeded\n")
+		}
+		return nil
+	}
+	return nil
 }
