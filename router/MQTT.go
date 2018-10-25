626528a38edaa456d9acbf354d387af6777d9b11
Rodchananat <rod8711@gmail.com>
2018-10-25 18:16:56 +0700

    :sparkles: handle mqtt requests

--- router/MQTT.go
+++ router/MQTT.go
@@ -1,19 +1,38 @@
 package router
 
 import (
-	"../common"
+	"bytes"
+	"fmt"
+	"time"
 
+	"../common"
 	"github.com/surgemq/message"
-	"github.com/surgemq/surgemq/service"
 )
 
-func MQTT() {
+func handleMessage(msg *message.PublishMessage) error {
+	parsedData := common.ParseJSON(bytes.Trim(msg.Payload(), "\x00"))
+	fmt.Println(parsedData)
+	return nil
+}
 
-	common.CheckErr("connect to MQTT", common.ConnectToMQTT())
+func handleSubscriptionComplete(msg, ack message.Message, err error) error {
+	// fmt.Printf("Subscribed: %s\nAck: %s\n", msg.Decode([]byte("utf-8")), ack.Decode([]byte("utf-8")))
+	fmt.Print(msg, ack)
+	common.CheckErr("OnSubComplete Handler", err)
+	return nil
+}
 
-	client := &service.Client{}
-	subMsg := message.NewSubscribeMessage()
-	subMsg.AddTopic([]byte("CUSmartFarm"), 1)
+// MQTT : intialize MQTT Client
+func MQTT() error {
+	for {
+		common.CheckErr("connect to MQTT", common.ConnectToMQTT())
 
-	subMsg.
+		subMsg := message.NewSubscribeMessage()
+		subMsg.AddTopic([]byte("CUSmartFarm"), 1)
+		common.CheckErr("Subscribing", common.MqttClient.Subscribe(subMsg, handleSubscriptionComplete, handleMessage))
+
+		fmt.Print("Connected.")
+		time.Sleep(10 * time.Second)
+		fmt.Print("Reconnecting")
+	}
 }
