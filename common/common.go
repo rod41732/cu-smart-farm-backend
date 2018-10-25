b870335987504b86ca77bbc2e0fe631d9f4b1704
Rodchananat <rod8711@gmail.com>
2018-10-25 18:18:13 +0700

    :sparkles: added method for using with mqtt, influxDB

--- common/common.go
+++ common/common.go
@@ -1,14 +1,19 @@
 package common
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/gin-gonic/gin"
+	"github.com/influxdata/influxdb/client/v2"
 	"github.com/surgemq/message"
 	"github.com/surgemq/surgemq/service"
 )
 
+// MqttClient : this is MQTT client that listen to server
 var MqttClient *service.Client
 
+// CheckErr : return true and print if error
 func CheckErr(source string, err error) bool {
 	if err != nil {
 		fmt.Printf("[ERR] IN %s || %s\n", source, err)
@@ -17,7 +22,7 @@ func CheckErr(source string, err error) bool {
 	return false
 }
 
-// ConnecttToMQTT : connects to mqtt server and return error if error
+// ConnectToMQTT : connects to mqtt server and return error if error
 func ConnectToMQTT() error {
 	if MqttClient != nil {
 		MqttClient.Disconnect()
@@ -25,14 +30,33 @@ func ConnectToMQTT() error {
 	MqttClient = &service.Client{}
 
 	msg := message.NewConnectMessage()
-	msg.SetUsername("admin")
-	msg.SetPassword("iyddyoot")
+	msg.SetUsername([]byte("admin"))
+	msg.SetPassword([]byte("iyddyoot"))
 	msg.SetWillQos(2)
 	msg.SetKeepAlive(120)
 	msg.SetVersion(3)
 	msg.SetClientId([]byte("smart-farm-backend"))
 	msg.SetWillTopic([]byte("backend-service"))
 	msg.SetWillMessage([]byte("backend: connecting.."))
+
 	// msg.SetCleanSession(true)
 	return MqttClient.Connect("tcp://164.115.27.177:1883", msg)
+}
+
+// ParseJSON : parse byte to json (gin.H)
+func ParseJSON(payload []byte) gin.H {
+	var jsonData gin.H
+	CheckErr("Parsing JSON", json.Unmarshal(payload, &jsonData))
+	return jsonData
+}
+
+// ConnectToInfluxDB : connect to influx DB and return client
+func ConnectToInfluxDB() client.Client {
+	influxConn, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "tcp://localhost:8086",
+		Username: "admin",
+		Password: "4fs,mg-0zv",
+	})
+	CheckErr("Influx Connection", err)
+	return influxConn
 }
