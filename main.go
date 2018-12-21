797b4accbae39e3cb15aa28fff47ab0080cd82e9
Rodchananat <rod8711@gmail.com>
2018-12-21 17:29:05 +0700

    temporary fix

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rod41732/cu-smart-farm-backend/api/middleware"
 	"github.com/rod41732/cu-smart-farm-backend/common"
+	"github.com/rod41732/cu-smart-farm-backend/mqtt"
 	"github.com/rod41732/cu-smart-farm-backend/router"
 )
 
@@ -12,7 +13,7 @@ func main() {
 	common.InitializeKeyPair()
 	middleware.Initialize()
 
-	go router.MQTT()
+	go mqtt.MQTT()
 
 	r := gin.Default()
 
